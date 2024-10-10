e16361f9269ef614ad0020e64674b82fd9f9b213
Alejandro Cabrera <cabrera888alejandro@gmail.com>
2024-10-10 14:54:13 -0600

    Base de datos y validar Usuarios

--- src/code/Usuario.java
+++ src/code/Usuario.java
@@ -5,7 +5,55 @@ public class Usuario {
     private String username;
     private String name;
     private String password;
+    private boolean tipoUser;
     
-   //Constructor
-    
+    //Cnstruct
+    public Usuario(int id, String username, String name, String password, boolean tipoUser) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.tipoUser = tipoUser;
+    }
+    //Getter and Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isTipoUser() {
+        return tipoUser;
+    }
+
+    public void setTipoUser(boolean tipoUser) {
+        this.tipoUser = tipoUser;
+    }
 }
